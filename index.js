32787ef1edc9dc42f4ff81aa93034ee856a040bc
Verified ✔ <bryansosa2005@icloud.com>
2020-05-21 02:09:53 -0500

    update 2

---
+++ index.js
@@ -0,0 +1,45 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token } = require('./config.json');
+
+Structures.extend('Guild', function(Guild) {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null,
+        volume: 1
+      };
+      this.triviaData = {
+        isTriviaRunning: false,
+        wasTriviaEndCalled: false,
+        triviaQueue: [],
+        triviaScore: new Map()
+      };
+    }
+  }
+  return MusicGuild;
+});
+
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: '531318764281790504' // change this to your Discord user ID
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Command Group'],
+    ['gifs', 'Gif Command Group'],
+    ['other', 'random types of commands group']
+  ])
+  .registerDefaultGroups()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+
+
+client.login(token);
